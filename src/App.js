3117e96d7b17520319530eaaa67aef38f256e306
nurbek <abduhamidovnurbek888@gmail.com>
2022-04-08 10:13:12 +0500

    Redux Tutorial

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import AddMovie from "./AddMovie";
 import "./App.css";
-import { MovieProvider } from "./MovieContext";
-import MovieList from "./MovieList";
-import Nav from "./Nav";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "./actions";
 
 function App() {
+  const counter = useSelector((state) => state.counter);
+  const isLogged = useSelector((state) => state.isLogged);
+  const dispatch = useDispatch();
+
   return (
-    <MovieProvider>
-      <div className="App">
-        <Nav />
-        <AddMovie />
-        <MovieList />
-      </div>
-    </MovieProvider>
+    <div className="App">
+      <h1>Counter : {counter}</h1>
+      <button onClick={() => dispatch(increment(5))}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      {isLogged ? <h3>Valuable Information I shouldn't see</h3> : ""}
+    </div>
   );
 }
 
